def0c02d2a784bb83c85b5382f5603116d55fa70
Parker Jones <pjones@sharpspring.com>
2018-06-21 14:55:33 -0400

    part 2 completed

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 const generateName = require('sillyname');
 require('./index.css'); // ADDED FOR CSS
 const hello = require('./hello');
+const icon = require('./icon.png');
 
 const name = generateName();
-let counterEl = document.getElementById('counter');
-let incrementEl = document.getElementById('increment');
+const counterEl = document.getElementById('counter');
+const incrementEl = document.getElementById('increment');
+const iconEl = document.getElementById('icon');
 
 incrementEl.addEventListener('click', function() {
     counterEl.innerHTML = (parseInt(counterEl.innerHTML, 10) + 1).toString();
@@ -12,3 +14,5 @@ incrementEl.addEventListener('click', function() {
 
 hello();
 window.console.log(name);
+
+iconEl.src = icon;
